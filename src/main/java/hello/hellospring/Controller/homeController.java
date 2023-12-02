5711c9f928b3afe402158918e41782e0911292cf
wooni <cloud0477@naver.com>
2023-12-03 00:33:44 +0900

    회원관리예제 ~ 웹 MVC개발 까지 완료

---
+++ src/main/java/hello/hellospring/Controller/homeController.java
@@ -0,0 +1,13 @@
+package hello.hellospring.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class homeController {
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
